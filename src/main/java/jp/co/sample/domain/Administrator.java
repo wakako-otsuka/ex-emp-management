1e32179b7289a44e7c95e84ac546ae0b8267ee63
wakako <wakako.otsuka@rakus-partners.co.jp>
2019-08-02 12:32:38 +0900

    ドメイン情報

---
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -0,0 +1,64 @@
+package jp.co.sample.domain;
+
+/**
+ * 
+ * 管理者情報を表すドメイン
+ * 
+ * @auther otsuka
+ * 
+ */
+
+public class Administrator {
+
+
+	private Integer id;
+	
+	private String name;
+	
+	private String mailAddress;
+	
+	private String password;
+	
+	@Override
+	public String toString() {
+		return "Administrator [id=" + id + ", name=" + name + ", mail Address=" + mailAddress + ", password=" + password + "]";
+	}
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+}
